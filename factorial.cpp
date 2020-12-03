52adc87b9dc30f27cb8fdc98c022155bbb211920
Mher <74150133+mhermelkonyan98@users.noreply.github.com>
2020-12-04 03:05:32 +0400

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,9 +1,15 @@
 #include "factorial.h"
+#include <iostream>
+#include <exception>
 
-unsigned factorial(unsigned n) {
-  if (n == 1 || n == 0) {
-    return n;
-  }
-  return n * factorial(n - 1);
+typedef unsigned long long ull;
+
+ull factorial(int a) {
+    if (a == 0 || a == 1) {
+        return 1;
+    }
+    if (a < 0 || a > 65) {
+        throw std::invalid_argument("a must be greather than 0 and less than 66");
+    }
+    return a * factorial(a - 1);
 }
-
